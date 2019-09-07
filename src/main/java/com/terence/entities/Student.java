1fd311b55ac1fe1e0c9c8fa2df4a273ccfafe3bf
Terence <1581231966@qq.com>
2019-09-07 11:07:43 +0800

    Initial commit

---
+++ src/main/java/com/terence/entities/Student.java
@@ -0,0 +1,44 @@
+package com.terence.entities;
+
+/*
+* Author: Terence-->in home*
+* Date: 2019-9-7*/
+public class Student {
+
+	private String name ;
+	private String age ;
+	private String school;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	@Override
+	public String toString() {
+		return "Student{" +
+				"name='" + name + '\'' +
+				", age='" + age + '\'' +
+				", school='" + school + '\'' +
+				'}';
+	}
+}
